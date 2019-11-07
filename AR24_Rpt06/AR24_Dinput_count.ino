682cf2118b8588e74d3af4661cfdea6639952c53
septempeccatis <48310115+septempeccatis@users.noreply.github.com>
2019-11-07 16:51:59 +0900

    Add files via upload

---
+++ AR24_Rpt06/AR24_Dinput_count.ino
@@ -0,0 +1,27 @@
+const int ledPin = 13;
+const int inputPin = 2;
+
+int count = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  pinMode(inputPin,INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int swInput = digitalRead(inputPin);
+  int ledOutput = digitalRead(ledPin);
+
+  if(swInput == LOW)
+  {
+    if(ledOutput) digitalWrite(ledPin,LOW);
+
+    else digitalWrite(ledPin,HIGH);    
+
+    ++count;
+    Serial.println(count);
+  };
+}
---
+++ AR24_Rpt06/AR24_Switch.ino
@@ -0,0 +1,41 @@
+const int ledPin = 13;
+const int inputPin = 2;
+
+int count = 0;
+String ledS[2] = {"LOW ","HIGH"};
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  pinMode(inputPin,INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int swInput = digitalRead(inputPin);
+  int ledOutput = digitalRead(ledPin);
+
+  if(swInput == LOW)
+  {
+    delay(400);
+    if(ledOutput)
+    {
+      Serial.print("LED : ");
+      Serial.print(ledS[ledOutput]);
+      delay(100);
+      digitalWrite(ledPin,LOW);
+    }
+    else
+    {
+      Serial.print("LED : ");
+      Serial.print(ledS[ledOutput]);
+      delay(100);
+      digitalWrite(ledPin,HIGH);    
+    }
+
+    ++count;
+    Serial.print(", count: ");
+    Serial.println(count);
+  };
+}
